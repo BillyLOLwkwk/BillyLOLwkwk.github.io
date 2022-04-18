26e92ab84803c269fc271c5b82670f4cb2c538b4
SMG-500 <atanasiusbrilliant@gmail.com>
2022-04-18 22:24:19 +0700

    new osis web update

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
+        youtube: "#FF0000",
+        instagram: "#E4405F",
+        facebook: "#1877F2",
+        home: "#F68315",
+        kontak: "#26A5E4",
+        anggota: "#809CC9",
+      },
+      screens: {
+        "2xl": "1320 px",
+      },
+    },
+  },
+  plugins: [],
+};
